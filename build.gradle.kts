09d1af91c08d8154f884d12c2cdacf14da8a2478
arnonjun <Arnonlove1886@gmail.com>
2026-09-25 14:15:13 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,36 @@
-plugins { id("com.android.application") version "8.7.3" apply false; id("org.jetbrains.kotlin.android") version "2.0.21" apply false }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.tsk.trofeomirror"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.tsk.trofeomirror"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 11
+        versionName = "1.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
